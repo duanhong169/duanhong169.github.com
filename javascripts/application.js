f468cd69b56b93961b5967ea1dd8de9c6ac517c6
Hong Duan <duanhong169@gmail.com>
2013-03-24 16:29:14 +0800

    使用jekyllbootstrap

--- javascripts/application.js
+++
@@ -1,3 +0,0 @@
-var $=function(a){return document.getElementById(a)};
-// If Supports Inline SVG, Display The Social Links
-(function(){var a=document.createElement("div");a.innerHTML="<svg/>",(a.firstChild&&a.firstChild.namespaceURI)!="http://www.w3.org/2000/svg"&&($("social-links").style.display="none")})();
